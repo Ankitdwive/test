55c6ba547fd82817e7ce83c23093cb105bdaf7cb
Ankitdwive <ankit@swrmhr.com>
2023-02-08 23:44:29 -0600

    git commit

--- Candidate/src/main/java/com/example/controller.java
+++ Candidate/src/main/java/com/example/controller.java
@@ -1,14 +1,24 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
-
+	
+	
 	@RequestMapping("/")
 	public String dwivedi() {
 		return "helloo Ankit kumar dwivedi ";
 		
 	}
+@GetMapping("/ankit")
+public String wivedi() {
+	return "helloo Ankit kumar dwivedi ";
+	
 }
+
+
+}
+
